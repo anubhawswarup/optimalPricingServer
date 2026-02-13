ad577dbe9f9ef12845f489d37811275987ceca32
Anubhaw Swarup <anu007swarup@gmail.com>
2026-02-14 04:58:59 +0900

    updating test script

--- testscript.sh
+++ testscript.sh
@@ -20,16 +20,3 @@ do
   # Sleep for 0.6 seconds to spread 100 calls over 1 minute
   sleep 0.6
 done
-
-
-
-#!/bin/bash
-# can disable redis and run this script to test performance without caching
-#URL="http://localhost:3000/api/v1/pricing?period=Winter&hotel=FloatingPointResort&room=SingletonRoom"
-
-#for i in {1..100}
-#do
-#   echo "Request $i"
-#   curl -s "$URL"
-#   sleep 0.6
-#done
